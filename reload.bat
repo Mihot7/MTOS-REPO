e33f1eadf1bbeb5c3690c577fb2abaeed2c5d317
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2024-08-31 11:14:48 +0200

    Update reload to version 2.0

--- reload.bat
+++ reload.bat
@@ -5,8 +5,9 @@ if %ver%==3.0 echo Not supported && %shell%
 if %ver%==4.0 echo Not supported && %shell%
 cd ..
 cd plugins
-echo reoading plugins...
-for %%G in (*.bat) do call "%%G" && set /a plugins=plugins+1
+echo Reloader version 2.0 by Lubie_cie
+echo Reloading plugins...
+for %%G in (*.bat) do call "%%G"
 timeout /t 2 /nobreak>nul
 cd ..
 cd prog
