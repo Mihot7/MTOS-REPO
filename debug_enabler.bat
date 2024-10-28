53523e50f1bb4db984cda94d28a8d3638a1b4135
Mihot7 <105102302+Mihot7@users.noreply.github.com>
2024-10-28 20:23:23 +0100

    Update debug_enabler.bat

--- debug_enabler.bat
+++ debug_enabler.bat
@@ -1,4 +1,17 @@
 echo Made by Mihot7
 echo TAK NAPRAWIAM BŁĘDY W TEN SPOSÓB XDDD
+if %ver%==2.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==2.1 echo VER NOT SUPORTED! && %shell%
+if %ver%==2.2 echo VER NOT SUPORTED! && %shell%
+if %ver%==3.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==4.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==5.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==5.1 echo VER NOT SUPORTED! && %shell%
+if %ver%==5.2 echo VER NOT SUPORTED! && %shell%
+if %ver%==6.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==6.1 echo VER NOT SUPORTED! && %shell%
+if %ver%==7.0 echo VER NOT SUPORTED! && %shell%
+if %ver%==7.1 echo VER NOT SUPORTED! && %shell%
 set debug=true
+echo DEBUG IS TURNED ON!
 %shell%
